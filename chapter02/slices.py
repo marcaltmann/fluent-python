8fd2d7ff9da4b172d1902075c9d02ea2e1c14fea
Marc Altmann <marcaltmann@posteo.de>
2026-02-07 00:26:37 +0100

    Add stuff on slices

---
+++ chapter02/slices.py
@@ -0,0 +1,27 @@
+"""
+Some interesting things about slices, although I almost never
+use them.
+
+Interesting: The article about the syntax.
+https://fpy.li/2-32
+
+Also interesting: The slice object, e.g.:
+"""
+a = [*range(10)]
+s = slice(2, 4)
+a[s]
+
+"""
+I don't know how useful the other slice stuff is for me.
+- The step/stride
+- The negative stride
+- Assigning to slices...
+
+- Maybe the del operator, e.g.
+"""
+del a[1:2]
+
+"""
+The Ellipsis object (...) is also interesting, but cannot be
+really used without NumPy multidimensional arrays etc.
+"""
